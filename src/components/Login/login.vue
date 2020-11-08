e88a5dde4594d225584d0242c1201f539b856bb7
ChunxiangYu <yu1462007467@gmail.com>
2020-11-08 15:56:12 +0800

    finished the design of login page. fixed the mobile devices.

---
+++ src/components/Login/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <form class="login-form">
+    <div id="topLogo">
+      <img id="logo" alt="" src="/EduSourceLogo.png" />
+    </div>
+    <form class="input-form">
+      <ul class="wrapper">
+        <li id="welcome">
+          <label class="welcome">Welcome back,</label>
+          <label class="welcome">Login here</label>
+        </li>
+        <li id="emailInput">
+          <label>
+            <input
+              class="inputLine"
+              placeholder="Email"
+              type="text"
+              v-model="email"
+            />
+          </label>
+          <label class="notice">Inactive</label>
+        </li>
+        <li id="passwordInput">
+          <label>
+            <input
+              class="inputLine"
+              placeholder="Password"
+              type="password"
+              v-model="password"
+              @keypress.enter="login"
+            />
+          </label>
+          <label class="notice">Hover</label>
+        </li>
+      </ul>
+    </form>
+    <div>
+      <div class="actions">
+        <button id="loginButton" type="button" @click="login">LOG IN</button>
+      </div>
+      <div class="actions">
+        <button id="signupButton" type="button" @click="signUp">SIGN UP</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<style lang="less" src="./login.less"></style>
+<script lang="ts" src="./login.ts"></script>
